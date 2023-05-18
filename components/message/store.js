28e04d462b8c54131d884fa54acb612776df98fe
Santiago Eguidazu <eguidazu.san@gmail.com>
2023-05-18 18:28:41 -0300

    class number 20 of node course mongodb and websockets

--- components/message/store.js
+++ components/message/store.js
@@ -1,11 +1,27 @@
-const list = [];
+const db = require("mongoose");
+const Model = require("./model");
+
+const uri =
+  "mongodb+srv://seguidazu:yA6F1flTSNmjJsZz@cluster0.z1vnevp.mongodb.net/telegrom?retryWrites=true&w=majority";
+
+db.Promise = global.Promise;
+db.connect(uri)
+  .then(() => {
+    console.log("[db] connected successfully");
+  })
+  .catch((err) => {
+    console.error("[dbError]", err);
+  });
 
 function addMessage(message) {
-  list.push(message);
+  // list.push(message);
+  const myMessage = new Model(message);
+  myMessage.save();
 }
 
-function getMessages() {
-  return list;
+async function getMessages() {
+  const messages = await Model.find();
+  return messages;
 }
 
 module.exports = { add: addMessage, list: getMessages };
