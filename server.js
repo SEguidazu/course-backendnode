8b52d78f4385663d9c413ad8d9d5b8f73d61d40d
Santiago Eguidazu <eguidazu.san@gmail.com>
2023-05-20 00:11:13 -0300

    class number 31 of node course mongodb and websockets

--- server.js
+++ server.js
@@ -1,20 +1,25 @@
 const express = require("express");
-const bodyParser = require("body-parser");
+const app = express();
+const server = require("http").Server(app);
 
+const bodyParser = require("body-parser");
+const socket = require("./socket");
 const db = require("./db");
 const router = require("./network/routes");
 
 db(
   "mongodb+srv://seguidazu:yA6F1flTSNmjJsZz@cluster0.z1vnevp.mongodb.net/telegrom?retryWrites=true&w=majority"
 );
-const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+socket.connect(server);
 
 router(app);
 
 app.use("/app", express.static("public"));
 
-app.listen(3000);
-console.log("la aplicacion esta escuchando en http://localhost:3000");
+server.listen(3000, function () {
+  console.log("la aplicacion esta escuchando en http://localhost:3000");
+});
